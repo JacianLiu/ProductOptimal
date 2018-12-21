e0612e367a8c42c2a7b94cac136d715e1ba1ac9c
JacianLiu <jacianliu@gmail.com>
2018-12-21 16:12:04 +0800

    域名映射

--- pinyougou_portal_web/src/main/webapp/js/controller/indexController.js
+++ pinyougou_portal_web/src/main/webapp/js/controller/indexController.js
@@ -6,5 +6,10 @@ app.controller("indexController",function ($scope,$controller,contentService) {
         contentService.findByCategoryId(categoryId).success(function (response) {
             $scope.contentList = response;
         });
+    };
+
+    // 门户网站与搜索页对接
+    $scope.search = function () {
+        location.href = "http://search.pinyougou.com/search.html#?keywords=" + $scope.keywords;
     }
 });
