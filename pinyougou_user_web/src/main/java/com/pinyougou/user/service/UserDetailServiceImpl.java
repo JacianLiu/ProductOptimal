47cb3925c4ab352db47007548a7e5ed75b79c376
RickyLiuJ <RickyLiuJ@163.com>
2018-12-24 11:24:34 +0800

    CAS单点登录

---
+++ pinyougou_user_web/src/main/java/com/pinyougou/user/service/UserDetailServiceImpl.java
@@ -0,0 +1,39 @@
+package com.pinyougou.user.service;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @author Ricky
+ * @date create in 2018/12/24
+ * TODO
+ */
+public class UserDetailServiceImpl implements UserDetailsService {
+    /**
+     * Locates the user based on the username. In the actual implementation, the search
+     * may possibly be case sensitive, or case insensitive depending on how the
+     * implementation instance is configured. In this case, the <code>UserDetails</code>
+     * object that comes back may have a username that is of a different case than what
+     * was actually requested..
+     *
+     * @param username the username identifying the user whose data is required.
+     * @return a fully populated user record (never <code>null</code>)
+     * @throws UsernameNotFoundException if the user could not be found or the user has no
+     *                                   GrantedAuthority
+     */
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        // 认证服务类 , 认证操作交给 CAS 完成 , spring security 完成授权操作
+
+        List<GrantedAuthority> authorities = new ArrayList<>();
+        authorities.add(new SimpleGrantedAuthority("ROLE_USER"));
+        return new User(username, "", authorities);
+    }
+}
